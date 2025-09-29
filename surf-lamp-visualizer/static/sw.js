60d9ad3b2d89bfdb3108e0c89cf6c5841dd8a110
shahar42 <nitzanshahar42@gmail.com>
2025-09-30 00:21:54 +0300

    Add PWA support - installable app with manifest and service worker

---
+++ surf-lamp-visualizer/static/sw.js
@@ -0,0 +1,20 @@
+// Service Worker for PWA
+const CACHE_NAME = 'surf-lamp-v1';
+const urlsToCache = [
+  '/',
+  '/static/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
