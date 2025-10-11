b59ddd374f969c2bdff0805edc3d2e3a0044e6d0
shahar42 <nitzanshahar42@gmail.com>
2025-10-11 16:46:45 +0300

    General Update

--- run_daily_summary.sh
+++ run_daily_summary.sh
@@ -3,5 +3,5 @@
 # Generates AI-powered daily insights and sends comprehensive email report
 
 cd /home/shahar42/Git_Surf_Lamp_Agent
-source myenv/bin/activate
+source esurf/bin/activate
 python3 run_daily_insights.py >> insights_cron.log 2>&1
