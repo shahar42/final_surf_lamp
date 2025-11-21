8693046ce0f9290c7b4ba47db6c6ec287a458925
shahar42 <nitzanshahar42@gmail.com>
2025-11-21 21:37:32 +0200

    fix: Disable Flask template caching for PWA updates

--- surf-lamp-visualizer/app.py
+++ surf-lamp-visualizer/app.py
@@ -11,6 +11,8 @@ import markdown2
 load_dotenv()
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 CORS(app)
 
 # System architecture data
