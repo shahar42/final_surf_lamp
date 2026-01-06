cf9e752a3508495ad6540626845dc0506fb500d6
shahar42 <nitzanshahar42@gmail.com>
2026-01-06 13:01:51 +0200

    fix: add decoupled config for background processor

---
+++ surf-lamp-processor/config.py
@@ -0,0 +1,18 @@
+# Configuration for Background Processor
+# Decoupled from web app dependencies
+
+SURF_LOCATIONS = [
+    "Hadera, Israel",
+    "Tel Aviv, Israel", 
+    "Ashdod, Israel",
+    "Haifa, Israel",
+    "Netanya, Israel",
+    "Ashkelon, Israel",
+    "Nahariya, Israel"
+]
+
+BRIGHTNESS_LEVELS = {
+    'LOW': 0.05,
+    'MID': 0.3,
+    'HIGH': 1.0
+}
