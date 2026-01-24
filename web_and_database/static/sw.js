85361108765ebacde6045262f6da535059254672
shahar42 <nitzanshahar42@gmail.com>
2026-01-25 00:11:07 +0200

    fix(pwa): Add monochrome badge icon for correct Android notification rendering

--- web_and_database/static/sw.js
+++ web_and_database/static/sw.js
@@ -4,6 +4,7 @@ self.addEventListener('push', function(event) {
     const options = {
       body: data.body,
       icon: '/static/icon-192.png',
+      badge: '/static/badge-icon.png',
       vibrate: [100, 50, 100],
       data: {
         dateOfArrival: Date.now(),
